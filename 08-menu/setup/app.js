85de814a44376ccf3587f963fbd0a1b69faf3120
Sarita Shera <sarita.shera@gmail.com>
2022-03-23 18:21:57 -0700

    Completed 15 JavaScript projects.

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,76 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const container = document.querySelector(".btn-container");
+
+// load items
+
+window.addEventListener('DOMContentLoaded', function(){
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems (menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt=${item.title}>
+    <div class="item-info">
+      <header><h4>${item.title}</h4>
+      <h4 class="price">${item.price}</h4></header>
+      <p class="item-text">${item.desc}}</p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons(){ 
+  const categories = menu.reduce(function (values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category);
+    }
+    return values;
+    }, ['all']);
+    const categoryBtns = categories.map(function(category){
+    return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`;
+  }).join('');
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  filterBtns.forEach(function (btn){
+    btn.addEventListener('click', function(e){
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem){
+        if(menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if(category === 'all'){
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+}
+
+// filter items
+
+
+
+// get only unique categories - hardest part
+// iterate over categories, return buttons
+// make sure to select buttons when they are available
+
+
--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,70 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2022, 4, 25, 11, 30, 0);
+// month is 0-index based
+// hours are 0-24
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+const year = futureDate.getFullYear();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+giveaway.textContent = `giveaway ends on ${weekday}, ${month} ${date} ${year} @ ${hour}:${minutes}am`;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(t/oneDay);
+  let hrs = Math.floor((t % oneDay)/oneHour);
+  let mins = Math.floor((t % oneHour)/oneMinute);
+  let seconds = Math.floor((t % oneMinute)/1000);
+
+  //set values array
+  const values = [days, hrs, mins, seconds];
+
+  function format(item){
+    if(item < 10){
+      return item = `0${item}`
+    } else {
+      return item;
+    }
+  }
+
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index]);
+  });
+  
+  if(t < 0){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`
+  }
+}
+
+//countdown
+let countdown = setInterval(getRemainingTime,1000);
+
+getRemainingTime();
+
